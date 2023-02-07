bb340845bf1f6ddfd990355b15a460d6d534838a
ankitsingh0606 <ankitsingh10026@gmail.com>
2023-02-07 10:21:36 +0530

    finally complete ticket screen

--- lib/screen/ticket_screen.dart
+++ lib/screen/ticket_screen.dart
@@ -145,7 +145,37 @@ class TicketScreen extends StatelessWidget {
                 child: TicketView(showTicket: ticketList[0],),
               ),
             ],
-          )
+          ),
+          Positioned(
+            left: AppLayout.getHeight(21),
+            top: AppLayout.getHeight(295),
+            child: Container(
+              padding: EdgeInsets.all(3),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(color: Styles.textColor, width: 2),
+              ),
+              child: CircleAvatar(
+                maxRadius: 4,
+                backgroundColor: Styles.textColor,
+              ),
+            ),
+          ),
+          Positioned(
+            right: AppLayout.getHeight(21),
+            top: AppLayout.getHeight(295),
+            child: Container(
+              padding: EdgeInsets.all(3),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(color: Styles.textColor, width: 2),
+              ),
+              child: CircleAvatar(
+                maxRadius: 4,
+                backgroundColor: Styles.textColor,
+              ),
+            ),
+          ),
         ],
       )
     );
